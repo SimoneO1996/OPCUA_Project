73aae3461fd452933132d16ec816e439756a5867
Simone Onesta <simone.onesta96@gmail.com>
2020-05-31 11:32:56 +0200

    initial commit

---
+++ Client/client.ts
@@ -0,0 +1,69 @@
+const opcua = require("node-opcua");
+const filetransfer = require("node-opcua-file-transfer");
+
+const AttributeIds = opcua.AttributeIds;
+const OPCUAClient = opcua.OPCUAClient;
+const ClientFile = filetransfer.ClientFile;
+const OpenFileMode = filetransfer.OpenFileMode;
+const client = OPCUAClient.create({});
+const endpointUrl = "opc.tcp://DESKTOP-K17M86F:4334/UA/Prova";
+
+async function main() {
+  try {
+    // step 1 : connect to
+    await client.connect(endpointUrl);
+    console.log("connected !");
+
+    // step 2 : createSession
+    const session = await client.createSession();
+    console.log("session created !");
+
+    // step 3 : browse
+    const browseResult = await session.browse("RootFolder");
+
+    console.log("references of Root :");
+    for (const reference of browseResult.references) {
+      console.log("   -> ", reference.browseName.toString());
+      console.log(reference);
+      if (reference.browseName.toString() == "Objects") {
+        for (const r of reference) {
+          console.log("references of Object :");
+          console.log("   -> ", reference.browseName.toString());
+        }
+      }
+    }
+
+    const fileNodeId = "ns=1;s=MyFile";
+
+    // let's create a client file object from the session and nodeId
+    const clientFile = new ClientFile(session, fileNodeId);
+
+    // let's open the file
+    const mode = OpenFileMode.ReadWriteAppend;
+    await clientFile.open(mode);
+
+    // ... do some reading or writing
+    //const size = await clientFile.size();
+    //console.log("the current file size is : ", size, " bytes");
+    const pos = await clientFile.setPosition([0,0]);
+    console.log(pos);
+    const data: Buffer = await clientFile.read(6);
+    console.log("What can I say except ", data.toString());
+    // don't forget to close the file when done
+    await clientFile.close();
+
+    // step 4 : read a variable with readVariableValue
+    
+
+    // close session
+    await session.close();
+
+    // disconnecting
+    await client.disconnect();
+    console.log("done !");
+  }
+  catch (err) {
+    console.log("An error has occured : ",err);
+  }
+}
+main();
---
+++ Server/server.ts
@@ -0,0 +1,141 @@
+let opcua = require("node-opcua");
+let file_transfer = require("node-opcua-file-transfer");
+
+let date_ob = new Date();
+let server = new opcua.OPCUAServer({
+    port: 4334,
+    resourcePath: "/UA/Prova",
+    buildInfo: {
+        productName: "RaspUAServer1",
+        buildNumber: "1",
+        buildDate: new Date()
+    }
+});
+
+server.initialize(() =>{
+    console.log("OPC UA Server initialized.");
+
+    function build_my_address_space(server) {
+        const addressSpace = server.engine.addressSpace;
+        const namespace = addressSpace.getOwnNamespace();
+
+        //declare a new object
+        let device = namespace.addObject({
+            organizedBy: addressSpace.rootFolder.objects,
+            browseName: "MyDevice"
+        });
+
+        const fileType = addressSpace.findObjectType("FileType")!;
+
+        const myFile = fileType.instantiate({
+            nodeId: "s=MyFile",
+            browseName: "MyFile",
+            organizedBy: addressSpace.rootFolder.objects
+        });
+
+        file_transfer.installFileType(myFile, { 
+            filename: "prova.txt"
+        });
+
+        const method = namespace.addMethod(myFile,{
+
+            browseName: "Bark",
+        
+            inputArguments:  [
+                {
+                    name:"nbBarks",
+                    description: { text: "specifies the number of time I should bark" },
+                    dataType: opcua.DataType.UInt32        
+                },{
+                    name:"volume",
+                    description: { text: "specifies the sound volume [0 = quiet ,100 = loud]" },
+                    dataType: opcua.DataType.UInt32
+                }
+             ],
+        
+            outputArguments: [{
+                 name:"Barks",
+                 description:{ text: "the generated barks" },
+                 dataType: opcua.DataType.String ,
+                 valueRank: 1
+            }]
+        });
+        
+        // optionally, we can adjust userAccessLevel attribute 
+        method.outputArguments.userAccessLevel = opcua.makeAccessLevelFlag("CurrentRead");
+        method.inputArguments.userAccessLevel = opcua.makeAccessLevelFlag("CurrentRead");
+
+
+        method.bindMethod((inputArguments,context,callback) => {
+
+            const nbBarks = inputArguments[0].value;
+            const volume =  inputArguments[1].value;
+        
+            console.log("Hello World ! I will bark ",nbBarks," times");
+            console.log("the requested volume is ",volume,"");
+            const sound_volume = Array(volume).join("!");
+        
+            const barks = [];
+            for(let i=0; i < nbBarks; i++){
+                barks.push("Whaff" + sound_volume);
+            }
+        
+            const callMethodResult = {
+                statusCode: opcua.StatusCodes.Good,
+                outputArguments: [{
+                        dataType: opcua.DataType.String,
+                        arrayType: opcua.VariantArrayType.Array,
+                        value :barks
+                }]
+            };
+            callback(null,callMethodResult);
+        });
+
+        //add some variables
+        namespace.addVariable({
+            propertyOf: device,
+            browseName: "prova1",
+            dataType: "String",
+            value: {
+                get: function() { 
+                    return new opcua.Variant({ 
+                        dataType: opcua.DataType.String, 
+                        value: "prova"
+                    });
+                }
+            }
+        });
+
+        namespace.addVariable({
+            propertyOf: device,
+            browseName: "seconds",
+            dataType: "String",
+            value: {
+                get: function() {
+                    return new opcua.Variant({
+                        dataType: opcua.DataType.Int32,
+                        value: date_ob.getSeconds()
+                    });
+                }
+            }
+        });
+
+
+    }
+
+    build_my_address_space(server);
+
+    console.log("Address space initialized.");
+
+    server.start(() => {
+        console.log(`Server is now listening port ${server.endpoints[0].port}... (press CTRL+C to stop)`);
+        let endpointUrl = server.endpoints[0].endpointDescriptions()[0].endpointUrl;
+        console.log("The primary server endpoint url is ", endpointUrl);
+    });
+
+});
+
+process.on('SIGINT', function () {
+    process.exit(0);
+});
+
